e2b2d88e07283454fbaa27822c4fab14d22a606c
Russel Winder <russel@winder.org.uk>
2012-10-29 00:21:36 +0000

    A few changes of name of fields. Simplification of the collision detection and processing.

--- src/main/groovy/uk/org/winder/groovyballs/Application.groovy
+++ src/main/groovy/uk/org/winder/groovyballs/Application.groovy
@@ -22,14 +22,14 @@ import javafx.animation.AnimationTimer
 import static groovyx.javafx.GroovyFX.start
 
 arenaHeight = 300.0
-arenaWidth = 300.0
+arenaWidth = 400.0
 
-radius = 10
-location = radius / 2 + 0.1
+radius = 10.0
+offset = radius / 2.0 + 10.0
 
 balls = [
-	green: [glyph: null, data: new BallModel(x: location, y: location, r:10, dx: 1.0, dy: 0.8)],
-	red: [glyph: null, data: new BallModel(x: location, y: arenaHeight - location, r: 10, dx: 0.8, dy: -1.0)],
+	green: [glyph: null, data: new BallModel(x: offset, y: offset, r: radius, dx: 1.0, dy: 0.8)],
+	red: [glyph: null, data: new BallModel(x: offset, y: arenaHeight - offset, r: radius, dx: 0.8, dy: -1.0)],
 	]
 
 start {
@@ -38,17 +38,12 @@ start {
             balls.green.glyph = circle(centerX: bind(balls.green.data.x()), centerY: bind(balls.green.data.y()), radius: bind(balls.green.data.r())) { fill GREEN }
             balls.red.glyph = circle(centerX: bind(balls.red.data.x()), centerY: bind(balls.red.data.y()), radius: bind(balls.red.data.r())) { fill RED }
                 collisionCheck = { ->
-
-                    if (balls.green.data.x + balls.green.data.r > arenaWidth) balls.green.data.dx = -balls.green.data.dx
-                    if (balls.green.data.x - balls.green.data.r < 0) balls.green.data.dx = -balls.green.data.dx
-                    if (balls.red.data.x + balls.red.data.r > arenaWidth) balls.red.data.dx = -balls.red.data.dx
-                    if (balls.red.data.x - balls.red.data.r < 0) balls.red.data.dx = -balls.red.data.dx
-
-                    if (balls.green.data.y + balls.green.data.r > arenaHeight) balls.green.data.dy = -balls.green.data.dy
-                    if (balls.green.data.y - balls.green.data.r < 0) balls.green.data.dy = -balls.green.data.dy
-                    if (balls.red.data.y + balls.red.data.r > arenaHeight) balls.red.data.dy = -balls.red.data.dy
-                    if (balls.red.data.y - balls.red.data.r < 0) balls.red.data.dy = -balls.red.data.dy
-
+                    balls.each { key, value ->
+                        if (value.data.x + value.data.r >= arenaWidth || value.data.x - value.data.r <= 0) { value.data.dx = -value.data.dx }
+                        value.data.x += value.data.dx
+                        if (value.data.y + value.data.r >= arenaHeight || value.data.y - value.data.r <= 0) { value.data.dy = -value.data.dy }
+                        value.data.y += value.data.dy
+                    }
                     if (balls.green.data.intersects(balls.red.data)) {
                         balls.green.data.randomAngle()
                         balls.red.data.randomAngle()
@@ -56,13 +51,6 @@ start {
                 }
         }
     }
-	([handle: {collisionCheck()}] as AnimationTimer).start()
-    timeline(cycleCount: INDEFINITE, autoReverse: false) {
-        at(20.ms, onFinished: {
-            balls.green.data.x += balls.green.data.dx
-            balls.green.data.y += balls.green.data.dy
-            balls.red.data.x += balls.red.data.dx
-            balls.red.data.y += balls.red.data.dy
-        })
-    }.play()
+    ([handle: {collisionCheck()}] as AnimationTimer).start()
+    timeline(cycleCount: INDEFINITE, autoReverse: false).play()
 }
